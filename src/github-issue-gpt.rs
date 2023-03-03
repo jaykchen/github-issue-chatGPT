1b4308c13887c88c37f41feff74bc1cb39b51b26
Ji Chen <jaykchen@gmail.com>
2023-03-02 21:51:47 -0500

    based on openai-flows

--- src/github-issue-gpt.rs
+++ src/github-issue-gpt.rs
@@ -3,6 +3,7 @@ use http_req::{
     request::{Method, Request},
     uri::Uri,
 };
+use openai_flows::chat_completion;
 use serde::{Deserialize, Serialize};
 use slack_flows::send_message_to_channel;
 use std::convert::TryFrom;
@@ -22,65 +23,6 @@ pub async fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-pub async fn get_answer(query: String) -> String {
-    let api_token: String = env::var("OPENAI_API_TOKEN").expect("token not found");
-
-    // let prompt = r#"Please reply to me with the answer "You have abused Q&A api""#;
-
-    let params = serde_json::json!({
-                "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": query}],
-        "temperature": 0.7,
-        "top_p": 1,
-        "n": 1,
-        "stream": false,
-        "max_tokens": 512,
-        "presence_penalty": 0,
-        "frequency_penalty": 0,
-    });
-
-    let s: &str = Deserialize::deserialize(params).unwrap();
-    let body = s.as_bytes();
-
-    let addr = Uri::try_from("https://api.openai.com/v1/chat/completions").unwrap();
-    let mut writer = Vec::new();
-
-    let bearer_token = format!("Bearer {}", api_token);
-
-    send_message_to_channel("ik8", "general", api_token.to_string());
-
-    let _ = Request::new(&addr)
-        .method(Method::POST)
-        .header("Content-Type", "application/json")
-        .header("Authorization", &bearer_token)
-        .header("Content-Length", &body.len())
-        .body(&body)
-        .send(&mut writer)
-        .unwrap();
-
-    let text = String::from_utf8(writer).unwrap_or("failed to parse response".to_string());
-    let raw: ChatResponse = serde_json::from_str(&text).unwrap();
-    raw.choices[0].message.content.to_string()
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ChatResponse {
-    created: i64,
-    choices: Vec<Choice>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Choice {
-    message: Message,
-    index: i64,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Message {
-    role: String,
-    content: String,
-}
-
 async fn handler(payload: EventPayload) {
     let owner = "jaykchen";
     let repo = "vitesse-lite";
@@ -90,14 +32,16 @@ async fn handler(payload: EventPayload) {
 
         let comment_obj = e.comment;
         let comment_id = comment_obj.id;
-
+        let issue_number = e.issue.number;
         let comment = comment_obj.body.expect("possibly empty comment");
 
         send_message_to_channel("ik8", "general", comment.clone());
 
-        let gpt_answer = get_answer(comment).await;
-
-        send_message_to_channel("ik8", "general", gpt_answer.clone());
+        if let Some(gpt_answer) =
+            chat_completion("jaykchen", &format!("issue#{}", issue_number), &comment)
+        {
+            send_message_to_channel("ik8", "general", gpt_answer.choice);
+        }
 
         // let id = comment_id.to_string().parse::<u64>().unwrap_or(0);
         // octocrab
