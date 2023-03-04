967acbbd977d903a4b3f73a8a2f651d0013ca4bc
Ji Chen <jaykchen@gmail.com>
2023-03-03 20:23:46 -0500

    direct copy of example code

--- src/github-issue-gpt.rs
+++ src/github-issue-gpt.rs
@@ -1,3 +1,4 @@
+use flowsnet_platform_sdk::write_error_log;
 use github_flows::{get_octo, listen_to_event, EventPayload};
 use http_req::{
     request::{Method, Request},
@@ -8,7 +9,6 @@ use serde::{Deserialize, Serialize};
 use slack_flows::send_message_to_channel;
 use std::convert::TryFrom;
 use std::env;
-
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
@@ -27,45 +27,63 @@ async fn handler(payload: EventPayload) {
     let owner = "jaykchen";
     let repo = "vitesse-lite";
 
-    if let EventPayload::IssueCommentEvent(e) = payload {
-        // let octocrab = get_octo(None);
+    let octo = get_octo(Some(String::from("jaykchen")));
+    let issues = octo.issues(owner, repo);
 
-        let comment_obj = e.comment;
-        let comment_id = comment_obj.id;
-        let issue_number = e.issue.number;
-        let comment = comment_obj.body.expect("possibly empty comment");
-
-        send_message_to_channel("ik8", "general", comment.clone());
-
-        if let Some(gpt_answer) =
-            chat_completion("jaykchen", &format!("issue#{}", issue_number), &comment)
-        {
-            send_message_to_channel("ik8", "general", gpt_answer.choice);
+    match payload {
+        EventPayload::IssueCommentEvent(e) => {
+            if e.comment.user.r#type != "Bot" {
+                if let Some(b) = e.comment.body {
+                    if let Some(r) =
+                        chat_completion("jaykchen", &format!("issue#{}", e.issue.number), &b)
+                    {
+                        if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
+                            write_error_log!(e.to_string());
+                        }
+                    }
+                }
+            }
         }
-
-        // let id = comment_id.to_string().parse::<u64>().unwrap_or(0);
-        // octocrab
-        //     .issues(owner, repo)
-        //     .create_comment(id, gpt_answer)
-        //     .await;
-
-        // let mut writer = Vec::new();
-        // let query_str =
-        //     format!("repos/{owner}/{repo}/issues/{issue_number}/comments/{comment_id}/replies");
-        // let addr = Uri::try_from(query_str).unwrap();
-
-        // let body = serde_json::json!({ "body": gpt_answer });
-
-        // let _ = Request::new(&addr)
-        //     .method(Method::POST)
-        //     .header("Content-Type", "application/vnd.github.v3+json")
-        //     .header("Authorization", &bearer_token)
-        //     .header("Content-Length", &body.len())
-        //     .body(&body)
-        //     .send(&mut writer)
-        //     .unwrap();
-    }
+        _ => (),
+    };
 }
+// if let EventPayload::IssueCommentEvent(e) = payload {
+//     // let octocrab = get_octo(None);
+
+//     let comment_obj = e.comment;
+//     let comment_id = comment_obj.id;
+//     let issue_number = e.issue.number;
+//     let comment = comment_obj.body.expect("possibly empty comment");
+
+//     send_message_to_channel("ik8", "general", comment.clone());
+
+//     if let Some(gpt_answer) =
+//         chat_completion("jaykchen", &format!("issue#{}", issue_number), &comment)
+//     {
+//         send_message_to_channel("ik8", "general", gpt_answer.choice);
+//     }
+
+// let id = comment_id.to_string().parse::<u64>().unwrap_or(0);
+// octocrab
+//     .issues(owner, repo)
+//     .create_comment(id, gpt_answer)
+//     .await;
+
+// let mut writer = Vec::new();
+// let query_str =
+//     format!("repos/{owner}/{repo}/issues/{issue_number}/comments/{comment_id}/replies");
+// let addr = Uri::try_from(query_str).unwrap();
+
+// let body = serde_json::json!({ "body": gpt_answer });
+
+// let _ = Request::new(&addr)
+//     .method(Method::POST)
+//     .header("Content-Type", "application/vnd.github.v3+json")
+//     .header("Authorization", &bearer_token)
+//     .header("Content-Length", &body.len())
+//     .body(&body)
+//     .send(&mut writer)
+//     .unwrap();
 
 // async fn reply_comment(reply: String) {
 //     let mut writer = Vec::new();
